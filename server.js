8fdf4be93f47c9acdce7f3d8a0d38d4e0fd67c35
LEGEN DARY MAD <tajay59@gmail.com>
2019-04-23 20:42:51 -0500

    Updates heading on welcome page

--- server.js
+++ server.js
@@ -73,16 +73,16 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (req, res) {
+app.get('/tajay', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
     initDb(function(err){});
   }
   if (db) {
-    var col = db.collection('counts');
+    var col = db.collection('counting');
     // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
+    col.insert({name: "Legendary", date: Date.now()});
     col.count(function(err, count){
       if (err) {
         console.log('Error running count. Message:\n'+err);
